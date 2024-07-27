1911a3724903f4d840d9227a8e8cf22f960f80ed
gaurav081 <gauravkr081@gmail.com>
2024-07-27 17:47:27 +0530

    Initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,34 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { connectWallet } from '../services/blockchain'
-import { truncate, useGlobalState } from '../store'
-import { BsList, BsX } from 'react-icons/bs'
-import MobileHeader from './MobileHeader'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { connectWallet } from "../services/blockchain";
+import { truncate, useGlobalState } from "../store";
+import { BsList, BsX } from "react-icons/bs";
+import MobileHeader from "./MobileHeader";
 
 const Header = () => {
-  const [connectedAccount] = useGlobalState('connectedAccount')
-  const [isOpen, setIsOpen] = useState(false)
+  const [connectedAccount] = useGlobalState("connectedAccount");
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header className="bg-white w-full mx-auto p-5 flex justify-between items-center flex-wrap shadow-md">
-      <Link className="text-green-600 font-[risque] text-2xl" to={'/'}>
-        Dappworks
+      <Link className="text-green-600 font-[risque] text-2xl" to={"/"}>
+        FreeLanceNest
       </Link>
       <div className="items-center space-x-5 md:block hidden">
-        <Link to={'/mybids'} className="text-gray-600">
+        <Link to={"/mybids"} className="text-gray-600">
           My Bids
         </Link>
-        <Link to={'/myjobs'} className="text-gray-600">
+        <Link to={"/myjobs"} className="text-gray-600">
           My Jobs
         </Link>
-        <Link to={'/myprojects'} className="text-gray-600">
+        <Link to={"/myprojects"} className="text-gray-600">
           My Projects
         </Link>
-        <Link to={'/messages'} className="text-gray-600">
+        <Link to={"/messages"} className="text-gray-600">
           Messages
         </Link>
 
@@ -55,7 +55,7 @@ const Header = () => {
         <MobileHeader toggle={isOpen} />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
